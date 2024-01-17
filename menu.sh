4b483a5444fbafd61b65b95f80a4294282ff9808
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-17 21:49:58 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo -e " menu          =       Untuk Menampilkan semua Menu / perintah "
+echo -e " welcome       =       Untuk Menampilkan semua Menu Default "
+
+echo -e " add-ssh       =       untuk membuat akun ssh"
+echo -e " add-ssr       =       untuk membuat akun shadowsock"
+echo -e " add-vle       =       Untuk membuat Akun Vless"
+echo -e " add-vme       =       untuk Membuat Akun Vmess "
+echo -e " add-tro       =       Untuk membuat akun trojan "
+
+echo -e " trial-ssh     =       Untuk Membuat Akun Trial ssh"
+echo -e " trial-ssr     =       Untuk membuat akun Trial sadowsock"
+echo -e " trial-vle     =       Untuk membuat akun Trial vless "
+echo -e " trial-vme     =       Untuk membuat akun Trial vmess "
+echo -e " trial-tro     =       untuk membuat akun trial trojan "
+
+echo -e " renew-ssh     =       Untuk merenew akun ssh"
+echo -e " renew-vme     =       Untuk merenew akun vmess"
+echo -e " renew-vle     =       Untuk merenew akun vless"
+echo -e " renew-ssr     =       Untuk Merenew akun sadowsoock"
+echo -e " renew-tro     =       Untuk merenew akun trojan"
+
+
+echo -e " delet-ssh     =       Untuk menghapus akun ssh" # Delete Ssh Account
+echo -e " delet-vme     =       Untuk menghapus akun vmess" # Delete  vmess Account
+echo -e " delet-vle     =       Untuk menghapus akun vless" # Delete vless Account
+echo -e " delet-ssr     =       Untuk menghapus akun sadowsoock" # Delete Ssr Account
+echo -e " delet-tro     =       Untuk menghapus akun trojan" # Delete Trojan Account
+
+echo -e " ceklim        =       Untuk melihat member ssh" # Check ssh members
+echo -e " delet-xp      =       Untuk menghapus semua akun exp ssh" # Delet Expired Account
+
+echo -e " m-ftr         =       menu tambahan script" # Features
+echo -e " m-bot         =       menu bot telegram"    # Bot Tele
+echo -e " m-vme         =       menu vmess"           # Vmess xray
+echo -e " m-vle         =       menu vless"           # Vless xray
+echo -e " m-ssh         =       menu ssh openvpn"     # Ssh / openvpn
+echo -e " m-tro         =       menu trojan"          # Trojan Xray 
+echo -e " m-ssr         =       menu shadowsocks"     # Ssr-Libev
+echo -e " m-fix         =       menu fix domain"      # Fixcert Domain  
+echo -e " m-rst         =       menu Restart Service" # Restart Service
+echo -e " m-run         =       menu running Service" # Running Service
+echo -e " m-ipv         =       menu add ipvps ke github" # Registrasi Ipvps
+echo -e " m-udp         =       menu install udp custom"  # Install Udp
+echo -e " m-upd         =       menu Update Script"  # Update Script
+echo -e " m-lim         =       menu Limit xray & ssh " # Changes Limit ip
+echo -e " m-bkp         =       menu Backup" # Backup Data Vps
+
+echo -e " m-me          =       menu Menampilkan Author script" # About
+echo -e " m-prt         =       menu Menampilkan Port All Account" # Check Port
+echo -e " exit          =       EXIT " # EXIT 
